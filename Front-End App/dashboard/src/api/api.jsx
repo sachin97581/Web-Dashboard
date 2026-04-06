999c27f8680d511fba700774375811102fbc4639
sachin97581 <sachin97581@gmail.com>
2026-04-06 09:02:40 +0530

    add backend url

--- Front-End App/dashboard/src/api/api.jsx
+++ Front-End App/dashboard/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // your backend URL
+  baseURL: "https://web-dashboard-uilj.onrender.com", // your backend URL
 });
 
 export default API;
